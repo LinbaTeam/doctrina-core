1b2294b63c2fb67ca132fbb05d35f75fc814e73f
Ilya Kuteev <ilyakuteev@yandex-team.ru>
2023-05-13 17:25:25 +0300

    Init package

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+  name: "doctrina-core",
+  platforms: [
+    .macOS(.v10_15), .iOS(.v15),
+  ],
+  products: [],
+  dependencies: [],
+  targets: []
+)
